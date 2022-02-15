0244d1cf5f9ada8cd9d20833d9cab271584dac6c
Colin Hill <angrywookie@gmail.com>
2022-02-15 15:35:44 -0800

    update tests

--- terrapyn/__init__.py
+++ terrapyn/__init__.py
@@ -1,13 +1,4 @@
 """terrapyn namespace."""
 
-from importlib_metadata import PackageNotFoundError, version
-
 __author__ = "Colin Hill"
 __email__ = "colinalastairhill@gmail.com"
-
-# Used to automatically set version number from github actions
-# as well as not break when being tested locally
-try:
-    __version__ = version(__package__)  # type: ignore
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "0.0.0"
