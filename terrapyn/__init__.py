a2af95d996d99dfdf15779ea6b57c45de2907b64
Colin Hill <angrywookie@gmail.com>
2022-03-04 11:08:30 -0800

    fix imports

--- terrapyn/__init__.py
+++ terrapyn/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Colin Hill"
 __email__ = "colinalastairhill@gmail.com"
 
-from . import dask_utils, indices, stats, time, utils, validation
+from . import dask_utils, indices, stats, time, utils
 
-__all__ = ["time", "utils", "stats", "validation", "indices", "dask_utils"]
+__all__ = ["time", "utils", "stats", "indices", "dask_utils"]
