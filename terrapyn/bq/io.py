afa98471dd5971f512914ba2430eb4f5c7143d4a
Colin Hill <colinalastairhill@gmail.com>
2025-01-14 13:41:47 -0800

    update config

--- terrapyn/bq/io.py
+++ terrapyn/bq/io.py
@@ -3,10 +3,10 @@ from google.cloud.exceptions import NotFound
 
 
 def check_table_exists(table_id, client=None):
-    if client is None:
-        client = bigquery.Client()
-    try:
-        client.get_table(table_id)
-        return True
-    except NotFound:
-        return False
+	if client is None:
+		client = bigquery.Client()
+	try:
+		client.get_table(table_id)
+		return True
+	except NotFound:
+		return False
